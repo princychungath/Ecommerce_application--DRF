a46682352885e47d21b466c7b97836054a66a040
princy <princyc1998@gmail.com>
2023-08-14 23:10:54 +0530

    commit

--- eccomerce_project/admin_api/admin.py
+++ eccomerce_project/admin_api/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Product,Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['id','product_name','description','price','quantity','image']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['id','category_name']
--- eccomerce_project/user_api/admin.py
+++ eccomerce_project/user_api/admin.py
@@ -3,9 +3,6 @@ from .models import User,CartItem,Order,OrderItem,Address,Profile
 
 admin.site.register(User)
 admin.site.register(CartItem)
-admin.site.register(Address)
-admin.site.register(Profile)
-
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -14,3 +11,12 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display=['order','product','quantity','price']
+
+@admin.register(Address)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','user','house_name','place','pin','mobile_number','address_is_default']
+
+@admin.register(Profile)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','user','profile_picture','mobile_number','created_at']
+
